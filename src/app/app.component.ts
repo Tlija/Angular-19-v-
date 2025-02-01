138f088f91cfb7292c35bf04031bd4ceb4ed3023
tlija hachem <tlijanihachem4@gmail.com>
2025-02-01 11:20:41 +0100

    function for  rxResource

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {NgOptimizedImage} from '@angular/common';
+import {rxResource} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [NgOptimizedImage, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular19v';
+
+  url = 'https://fakestoreapi.com/products';
+  products: any = [];
+  productsResource: any = [];
+
+  constructor(private http: HttpClient) {
+    this.fetchProducts()
+  }
+
+  // fetchProducts() {
+  //   this.http.get(this.url).subscribe((res) => {
+  //     this.products = res
+  //     console.log(this.products)
+  //   })
+  //
+  // }
+
+  fetchProducts() {
+
+    this.productsResource = rxResource(
+      {
+        loader: () => {
+          return this.http.get(this.url)
+        }
+      }
+    )
+
+  }
+
 }
